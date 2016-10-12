d6a006e0f0b75bc9b8e9b2ccb23a16cbbda1d8a7
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-12 13:03:32 -0700

    Created base file and random madlib templates. Also added bulletized compliments for greeting.

--- madlibs.py
+++ madlibs.py
@@ -1,4 +1,4 @@
-from random import choice
+from random import choice, sample
 
 from flask import Flask, render_template, request
 
@@ -32,11 +32,12 @@ def greet_person():
 
     player = request.args.get("person")
 
-    compliment = choice(AWESOMENESS)
+    compliments = sample(AWESOMENESS, 3)
+    # compliment = ", ".join(compliments)
 
     return render_template("compliment.html",
                            person=player,
-                           compliment=compliment)
+                           compliments=compliments)
 
 
 @app.route('/game')
@@ -50,6 +51,7 @@ def show_madlib_form():
     else:
         return render_template("goodbye.html")
 
+
 @app.route('/madlib', methods=["GET"])
 def show_madlib():
     """Show madlib game result"""
@@ -57,17 +59,16 @@ def show_madlib():
     # madlib_stories = ["There once was a <b>{{ color }} {{ noun }}</b> sitting in the Hackbright Lab. When <u>{{ person }}</u> went to pick it up, it burst into flames in a totally <i>{{ adjective }}</i> way.", "{{ person }}, the {{ color }} {{ noun }} was a very popular {{ noun }} -- the most {{ adjective }} {{ noun }}."]
 
     person = request.args.get('person')
-
     color = request.args.get('color')
-
     noun = request.args.get('noun')
-
     adjectives = request.args.getlist('adjectives')
 
+    madlib_choices = ["madlib1.html", "madlib2.html"]
+    madlib_text = choice(madlib_choices)
 
     # madlib_text = choice(madlib_stories)
 
-    return render_template("madlib.html", 
+    return render_template(madlib_text, 
                             person = person, 
                             color = color,
                             noun = noun, 
